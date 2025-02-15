7e2f913923af4310cd1b330b6eeaf12bf086df41
Jolin <jolinlodhiya@gmail.com>
2025-02-15 14:58:53 -0500

    run
    updated run.py file

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)  # Create a Flask application instance
-
-# Define a route for the root URL (/)
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+from backend import app
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run the app in debug mode for development
