b72cd0084e0016a42cfc1143ac0148c084647735
MarkeKy <134136127+MarkeKy@users.noreply.github.com>
2025-02-16 12:13:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from backend import app
 
 if __name__ == '__main__':
-    app.run(debug=True, , host='0.0.0.0')  # Run the app in debug mode for development
+    app.run(debug=True, host='0.0.0.0')  # Run the app in debug mode for development
